06495e259e685ae2573896c20cf23b631148ae04
e <wb-rc590796@alibaba-inc.com>
2024-08-22 10:18:39 +0800

    feat:build

--- vite.config.ts
+++ vite.config.ts
@@ -29,6 +29,27 @@ export default defineConfig({
       resolvers: [ElementPlusResolver()]
     })
   ],
+  build: {
+    outDir: 'docs'
+    // lib: {
+    //   entry: 'src/main.ts',
+    //   name: 'new-grand-chatgpt',
+    //   fileName: 'new-grand-chatgpt.js',
+    //   formats: ['umd']
+    // },
+    // sourcemap: false, // 输出.map文件
+    // rollupOptions: {
+    //   // 确保外部化处理那些你不想打包进库的依赖
+    //   external: ['vue'],
+    //   output: {
+    //     // 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量
+    //     globals: {
+    //       vue: 'Vue',
+    //       newGrandChatgpt: 'new-grand-chatgpt' // 这里暴露出去一个全局变量
+    //     }
+    //   }
+    // }
+  },
 
   resolve: {
     alias: {
